95cac0d7f84ae91f1a7891c79de2322ee7c2b3d6
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 21:57:22 +0300

    Updated setup.py and dictionary.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["SpellChecker/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-SpellChecker-Cy',
-    version='1.0.4',
+    version='1.0.5',
     packages=['SpellChecker'],
     package_data={'SpellChecker': ['*.pxd', '*.pyx', '*.c']},
     url='https://github.com/StarlangSoftware/TurkishSpellChecker-Cy',
@@ -13,5 +17,7 @@ setup(
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Turkish Spell Checker Library',
-    install_requires=['NlpToolkit-MorphologicalAnalysis-Cy', 'NlpToolkit-NGram-Cy']
+    install_requires=['NlpToolkit-MorphologicalAnalysis-Cy', 'NlpToolkit-NGram-Cy'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
