fc76a031e483e69b95eba9903086f4c5c8e9ea66
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-22 22:31:50 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["SpellChecker/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-SpellChecker-Cy',
-    version='1.0.5',
+    version='1.0.6',
     packages=['SpellChecker'],
     package_data={'SpellChecker': ['*.pxd', '*.pyx', '*.c']},
     url='https://github.com/StarlangSoftware/TurkishSpellChecker-Cy',
